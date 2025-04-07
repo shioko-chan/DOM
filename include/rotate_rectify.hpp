c123515cdf91136f7f2755db59f8414d5481cbd3
shioko <shioriaya@foxmail.com>
2025-04-07 03:34:20 +0000

    remove project backproject not use it otherwise but only in rotate_rectify.hpp
    save

--- include/rotate_rectify.hpp
+++ include/rotate_rectify.hpp
@@ -18,31 +18,6 @@ struct RectifyResult {
   PointsPipeline world2img;
 };
 
-template <std::ranges::range Range>
-inline auto backproject(const Range& img_points, const Pose& pose, const Intrinsic& intrinsic) {
-  return img_points | std::views::transform([&pose, &intrinsic](auto&& point) {
-           cv::Mat ray = pose.R() * intrinsic.K().inv() * point;
-           cv::normalize(ray, ray);
-           float       denominator = ray.at<float>(2, 0);
-           const float eps         = 0.1f;
-           if(std::abs(denominator) < eps) {
-             denominator = (denominator < 0 ? -eps : eps);
-           }
-           float   gamma = pose.altitude / denominator;
-           cv::Mat xyz_w = gamma * ray;
-           return Point<float>(xyz_w.at<float>(0, 0), xyz_w.at<float>(1, 0));
-         });
-}
-
-template <std::ranges::range Range>
-inline auto project(const Range& world_points, const Pose& pose, const Intrinsic& intrinsic) {
-  return world_points | std::views::transform([&pose, &intrinsic](auto&& point) {
-           cv::Mat xy_i =
-               intrinsic.K() * (cv::Mat_<float>(3, 1) << point.x / pose.altitude_ref, point.y / pose.altitude_ref, 1);
-           return Point<float>(xy_i.at<float>(0, 0), xy_i.at<float>(1, 0));
-         });
-}
-
 RectifyResult rotate_rectify(const cv::Size img_size, const Pose& pose, const Intrinsic& intrinsic, const cv::Mat& img) {
   auto [w, h] = img_size;
   if(w < 5 || h < 5) {
@@ -50,16 +25,29 @@ RectifyResult rotate_rectify(const cv::Size img_size, const Pose& pose, const In
   }
   cv::Mat mask = cv::Mat::ones(h - 4, w - 4, CV_8UC1) * 255;
   cv::copyMakeBorder(mask, mask, 2, 2, 2, 2, cv::BORDER_CONSTANT, cv::Scalar(0));
-  Points        src{Point<float>(w - 1, 0), Point<float>(0, 0), Point<float>(0, h - 1), Point<float>(w - 1, h - 1)};
-  auto          world_points     = backproject(src, pose, intrinsic);
-  auto          abs_world_points = world_points | std::views::transform([&pose](auto&& point) {
-                            return Point<float>(point.x + pose.coord.x, point.y + pose.coord.y);
-                          });
-  Point<float>  avg_point        = avg(world_points);
-  auto          camera_points    = world_points | std::views::transform([&avg_point](auto&& point) {
+  Points       src{Point<float>(w - 1, 0), Point<float>(0, 0), Point<float>(0, h - 1), Point<float>(w - 1, h - 1)};
+  auto         world_points  = src | std::views::transform([&pose, &intrinsic](auto&& point) {
+                        cv::Mat ray = pose.R() * intrinsic.K().inv() * point;
+                        cv::normalize(ray, ray);
+                        float       denominator = ray.at<float>(2, 0);
+                        const float eps         = 0.1f;
+                        if(std::abs(denominator) < eps) {
+                          denominator = (denominator < 0 ? -eps : eps);
+                        }
+                        float   gamma = pose.altitude / denominator;
+                        cv::Mat xyz_w = gamma * ray;
+                        return Point<float>(xyz_w.at<float>(0, 0), xyz_w.at<float>(1, 0));
+                      });
+  Point<float> avg_point     = avg(world_points);
+  auto         camera_points = world_points | std::views::transform([&avg_point](auto&& point) {
                          return Point<float>(point.x - avg_point.x, point.y - avg_point.y);
                        });
-  auto          img_points       = project(camera_points, pose, intrinsic);
+  auto         img_points =
+      camera_points | std::views::transform([&pose, &intrinsic](auto&& point) {
+        cv::Mat xy_i =
+            intrinsic.K() * (cv::Mat_<float>(3, 1) << point.x / pose.altitude_ref, point.y / pose.altitude_ref, 1);
+        return Point<float>(xy_i.at<float>(0, 0), xy_i.at<float>(1, 0));
+      });
   Points<float> dst(img_points.begin(), img_points.end());
   cv::Rect      rect = cv::boundingRect(dst);
   std::for_each(dst.begin(), dst.end(), [&rect](auto&& point) {
@@ -70,18 +58,24 @@ RectifyResult rotate_rectify(const cv::Size img_size, const Pose& pose, const In
   cv::Mat img_res, mask_res;
   cv::warpPerspective(img, img_res, M, cv::Size(rect.width, rect.height), cv::INTER_CUBIC);
   cv::warpPerspective(mask, mask_res, M, cv::Size(rect.width, rect.height), cv::INTER_CUBIC);
+  auto abs_world_points = world_points | std::views::transform([&pose](auto&& point) {
+                            return Point<float>(point.x + pose.coord.x, point.y + pose.coord.y);
+                          });
   return {
       .img         = std::move(img_res),
       .mask        = std::move(mask_res),
       .ground_span = Points<float>(abs_world_points.begin(), abs_world_points.end()),
       .world2img   = PointsPipeline([rect, avg_point, &pose, &intrinsic](const Points<float>& abs_world_points) {
-        auto camera_points =
+        auto img_points =
             abs_world_points | std::views::transform([&pose, &avg_point](auto&& point) {
               return Point<float>(point.x - pose.coord.x - avg_point.x, point.y - pose.coord.y - avg_point.y);
-            });
-        auto img_points = project(camera_points, pose, intrinsic) | std::views::transform([&rect](auto&& point) {
-                            return Point<float>(point.x - rect.x, point.y - rect.y);
-                          });
+            })
+            | std::views::transform([&pose, &intrinsic](auto&& point) {
+                cv::Mat xy_i = intrinsic.K()
+                               * (cv::Mat_<float>(3, 1) << point.x / pose.altitude_ref, point.y / pose.altitude_ref, 1);
+                return Point<float>(xy_i.at<float>(0, 0), xy_i.at<float>(1, 0));
+              });
+
         return Points<float>(img_points.begin(), img_points.end());
       })};
 }
