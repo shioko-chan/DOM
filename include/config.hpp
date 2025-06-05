4fc57c12b7123989d70dcf66f2748a6d66d99a9f
shioko <shioriaya@foxmail.com>
2025-06-05 21:28:34 +0000

    save

--- include/config.hpp
+++ include/config.hpp
@@ -28,7 +28,7 @@ enum method_t : std::uint8_t { SUPERPOINT, DISK };
 
 constexpr method_t FEATURE_EXTRACTION_METHOD{DISK};
 
-constexpr double GRID_LENGTH{1.0}; // meters
+constexpr double GRID_LENGTH{0.05}; // meters
 
 } // namespace SkyMerge
 
